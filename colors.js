d678703f13d5bab115f549b88f241d3a9038b1a3
Adam Salhi <adam06genius@gmail.com>
2021-09-16 10:32:40 +0000

    Initial commit

---
+++ colors.js
@@ -0,0 +1,18 @@
+// This file can be used with the themer CLI, see https://github.com/mjswensen/themer
+
+module.exports.colors = {
+  "dark": {
+    "shade0": "#1E222A",
+    "shade7": "#DEDEDE",
+    "accent0": "#E06C75",
+    "accent1": "#68829E",
+    "accent2": "#E5C07B",
+    "accent3": "#61AFEF",
+    "accent4": "#C678DD",
+    "accent5": "#98C379",
+    "accent6": "#98C379",
+    "accent7": "#C678DD"
+  }
+};
+
+// Your theme's URL: https://themer.dev/?colors.dark.shade0=%231e222a&colors.dark.shade7=%23dedede&colors.dark.accent0=%23e06c75&colors.dark.accent1=%2368829e&colors.dark.accent2=%23e5c07b&colors.dark.accent3=%2361afef&colors.dark.accent4=%23c678dd&colors.dark.accent5=%2398c379&colors.dark.accent6=%2398c379&colors.dark.accent7=%23c678dd&activeColorSet=dark
